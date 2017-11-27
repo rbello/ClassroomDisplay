4a40189a4cc992634add99ba964336e6c0eb8218
R. BELLO <remi@evolya.fr>
2017-11-27 21:21:07 +0000

    Doc

--- config/default.profile.php
+++ config/default.profile.php
@@ -1,7 +1,10 @@
 <?php
 
 return array(
+    // Nom long de l'établissement
     'nomEtablissement' => "Nom long de l'établissement",
+    // Code à deux lettres désignant l'établissement (autrement dit la racine analytique)
     'codeEtablissement' => "Code à deux lettres désignant l'établissement, autrement dit la racine analytique",
+    // ID des salles séparées par des espaces
     'salles' => ''
 );
